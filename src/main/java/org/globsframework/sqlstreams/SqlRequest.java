332bfcbdf23a1755a58c63021d01dbbfe49d3f80
MarcGuiot <marc.guiot@free.fr>
2020-10-06 11:03:31 +0200

    MaxSize annotation allow charset to be defined and truncate use it GlobHttpContent : add GlobHttpContent to send content without json conversion. SqlRequest  extends AutoClosable.

--- src/main/java/org/globsframework/sqlstreams/SqlRequest.java
+++ src/main/java/org/globsframework/sqlstreams/SqlRequest.java
@@ -2,7 +2,7 @@ package org.globsframework.sqlstreams;
 
 import org.globsframework.sqlstreams.exceptions.SqlException;
 
-public interface SqlRequest {
+public interface SqlRequest extends AutoCloseable {
     void run() throws SqlException;
 
     void close();
